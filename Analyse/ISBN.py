55108b50e34f409bbe3c5156c0f155982753e25d
Zeroxus <avalon.gwenda@gmail.com>
2018-02-25 22:26:04 +0800

    ISBN TO DO

---
+++ Analyse/ISBN.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+
+class ISBN:
+    def __init__(self):
+        pass
+
+    def getISBN(self):
+        pass
+
+    def getRates(self):
+        pass
+
+    def getCategory(self):
+        pass
